f7d966b6f25e3a805314ba121f0c79da4e29e564
sadums <samjadams4@gmail.com>
2023-03-29 09:37:17 -0600

    add search to search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+/* API CALLS */ 
 var API_KEY = "f958d5799c0e9323c09bec4b41b7efee";
 var limit = 1;
-var cityName = "London"
+var cityName;
 
 var geocodingRequestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${API_KEY}`;
 
@@ -11,10 +12,10 @@ var getCity = function(){
     .catch(error => console.log('error', error));
 }
 
-var latitude = 51.5085;
-var longitude = -0.1257;
+var latitude;
+var longitude;
 
-var weatherRequestURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+var weatherRequestURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
 
 var getWeather = function(){
     fetch(weatherRequestURL)
@@ -22,3 +23,29 @@ var getWeather = function(){
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 }
+
+
+/* SEARCH FOR CITY */ 
+var searchText = document.getElementById('searchInput');
+var searchButton = document.getElementById('searchButton');
+var searchHistory = document.getElementById('searchHistory');
+
+var addToHistory = function(search){
+    var searchCard = document.createElement("div");
+    searchCard.innerHTML = `
+    <div class="card text-white bg-secondary p-0 my-2">
+        <div class="card-body p-1 text-center">
+            <h4 class="card-title">${search}</h4>
+        </div>
+    </div>
+    `;
+    searchCard.setAttribute("id", search);
+
+    searchHistory.append(searchCard);
+}
+
+var search = function(event){
+    addToHistory(searchText.value);
+}
+
+searchButton.addEventListener("click", search);
